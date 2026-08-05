97e49979c1d517897d0dd82370c02da6fb499810
Bilal Shaikh <bilalshaikh1298@gmail.com>
2026-08-05 07:18:58 +0530

    added gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Instrument_Serif, Inter_Tight } from "next/font/google";
 import "./globals.css";
 import AttributionCapture from "@/components/AttributionCapture";
+
+const GTM_ID = "GTM-MB38CTSP";
 
 const instrumentSerif = Instrument_Serif({
   variable: "--font-instrument-serif",
@@ -48,7 +51,24 @@ export default function RootLayout({
       lang="en-AU"
       className={`${instrumentSerif.variable} ${interTight.variable}`}
     >
+      <head>
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
+      </head>
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <AttributionCapture />
         {children}
       </body>
